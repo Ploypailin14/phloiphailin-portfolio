d5747be885caf0543df839be4beea391da6ea1a9
Phloiphailin <ploypailinkumpook@gmail.com>
2026-08-16 23:03:03 +0700

    edit skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,13 +1,17 @@
 function Skills() {
   const skills = [
-    'HTML',
-    'CSS',
+    'HTML5',
+    'CSS3',
     'JavaScript',
     'React',
     'Vue.js',
     'Tailwind CSS',
+    'Bootstrap',
+    'Node.js',
+    'PostgreSQL',
     'Git',
     'GitHub',
+    'Figma',
   ]
 
   return (
@@ -15,7 +19,7 @@ function Skills() {
       <p className="section-label">MY SKILLS</p>
 
       <h1>Skills</h1>
-
+      {/*ใช้ map function เพื่อวนลูปผ่าน array ของ skills และสร้าง span สำหรับแต่ละ skill*/}
       <div className="skills-container">
         {skills.map((skill) => (
           <span className="skill-item" key={skill}>
